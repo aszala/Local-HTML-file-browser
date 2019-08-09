dd81e15092b6098fb3ed7d6771cd12f87be0e520
Abhay Zala <zala.abhay@gmail.com>
2019-08-09 01:47:41 -0400

    Initial Commit

---
+++ Local HTML File Browser/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Local_HTML_File_Browser
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fileViewer = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openDirectoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.srcCode = new System.Windows.Forms.TextBox();
+            this.toggleCodeBrowserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileViewer
+            // 
+            this.fileViewer.Dock = System.Windows.Forms.DockStyle.Left;
+            this.fileViewer.Location = new System.Drawing.Point(0, 40);
+            this.fileViewer.Name = "fileViewer";
+            this.fileViewer.Size = new System.Drawing.Size(326, 1056);
+            this.fileViewer.TabIndex = 0;
+            this.fileViewer.UseCompatibleStateImageBehavior = false;
+            this.fileViewer.View = System.Windows.Forms.View.List;
+            this.fileViewer.SelectedIndexChanged += new System.EventHandler(this.FileViewer_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.toggleCodeBrowserToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1404, 40);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openDirectoryToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 36);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openDirectoryToolStripMenuItem
+            // 
+            this.openDirectoryToolStripMenuItem.Name = "openDirectoryToolStripMenuItem";
+            this.openDirectoryToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.openDirectoryToolStripMenuItem.Text = "Open Directory";
+            this.openDirectoryToolStripMenuItem.Click += new System.EventHandler(this.OpenDirectoryToolStripMenuItem_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(326, 40);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(1078, 1056);
+            this.webBrowser.TabIndex = 2;
+            this.webBrowser.Visible = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // srcCode
+            // 
+            this.srcCode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.srcCode.Enabled = false;
+            this.srcCode.Location = new System.Drawing.Point(326, 40);
+            this.srcCode.Multiline = true;
+            this.srcCode.Name = "srcCode";
+            this.srcCode.Size = new System.Drawing.Size(1078, 1056);
+            this.srcCode.TabIndex = 4;
+            // 
+            // toggleCodeBrowserToolStripMenuItem
+            // 
+            this.toggleCodeBrowserToolStripMenuItem.Name = "toggleCodeBrowserToolStripMenuItem";
+            this.toggleCodeBrowserToolStripMenuItem.Size = new System.Drawing.Size(277, 36);
+            this.toggleCodeBrowserToolStripMenuItem.Text = "Toggle Code / Browser";
+            this.toggleCodeBrowserToolStripMenuItem.Click += new System.EventHandler(this.ToggleCodeBrowserToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label1.Location = new System.Drawing.Point(1143, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(261, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Developed By Abhay Zala";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1404, 1096);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.srcCode);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.fileViewer);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Main";
+            this.Text = "HTML File Viewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView fileViewer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openDirectoryToolStripMenuItem;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.ToolStripMenuItem toggleCodeBrowserToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox srcCode;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Local HTML File Browser/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Local_HTML_File_Browser {
+    public partial class Main : Form {
+        public Main() {
+            InitializeComponent();
+        }
+
+        bool useCode = false;
+        string selectedFilePath = "";
+
+        private void OpenDirectoryToolStripMenuItem_Click(object sender, EventArgs e) {
+            FolderBrowserDialog folderPicker = new FolderBrowserDialog();
+            if (folderPicker.ShowDialog() == DialogResult.OK) {
+
+                fileViewer.Items.Clear();
+
+                string[] files = Directory.GetFiles(folderPicker.SelectedPath);
+                foreach (string file in files) {
+
+                    string fileName = Path.GetFileNameWithoutExtension(file);
+
+                    if (file.Contains("htm")) {
+                        ListViewItem item = new ListViewItem(fileName);
+                        item.Tag = file;
+
+                        fileViewer.Items.Add(item);
+                    }
+                }
+
+            }
+        }
+
+        private void FileViewer_SelectedIndexChanged(object sender, EventArgs e) {
+            if (fileViewer.SelectedItems.Count > 0) {
+
+                ListViewItem selected = fileViewer.SelectedItems[0];
+                string selectedFilePath = selected.Tag.ToString();
+                this.selectedFilePath = selectedFilePath;
+
+                load();
+            }
+        }
+
+        private void load() {
+            if (selectedFilePath.Length < 1)
+                return;
+
+            if (useCode) {
+                srcCode.Text = "";
+
+                var lines = File.ReadLines(selectedFilePath);
+
+                foreach (var line in lines) {
+                    srcCode.AppendText(line.ToString());
+                    srcCode.AppendText(Environment.NewLine);
+                }
+
+                webBrowser.Visible = false;
+                srcCode.Visible = true;
+            } else {
+                webBrowser.Url = new Uri("file:///" + selectedFilePath);
+                webBrowser.Visible = true;
+                srcCode.Visible = false;
+            }
+        }
+
+        private void ToggleCodeBrowserToolStripMenuItem_Click(object sender, EventArgs e) {
+            useCode = !useCode;
+            load();
+        }
+    }
+}
